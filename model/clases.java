32d8c83a7240909417c127d499294156f97fd578
Saul <thesimulgamer007@gmail.com>
2025-04-29 20:37:07 +0100

    empezando con las clases

---
+++ model/clases.java
@@ -0,0 +1,81 @@
+package model;
+
+abstract class perso {
+
+    protected String nombre;
+    protected int nivel;
+    protected int salud;
+
+    abstract void atacar();
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+
+    public int getSalud() {
+        return salud;
+    }
+
+    public void setSalud(int salud) {
+        this.salud = salud;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "personaje: " + nombre + "nivel: " + nivel + "salud: " + salud;
+    }
+
+    //falta clases personalizadas equals() y HashCode() ademas de terminar con las clases de los personajes
+
+}
+
+public class clases {
+
+    class fisico {
+    
+       
+        
+    }
+    class magico {
+    
+       
+        
+    }
+
+    class guerrero extends fisico {
+    
+    
+
+    }
+
+    class mago extends magico {
+
+        
+
+    }
+    
+    class asesino extends fisico {
+
+
+
+    }
+
+
+
+}
+
---
+++ model/estado.java
@@ -0,0 +1,11 @@
+package model;
+
+public enum estado {
+
+    SALUDABLE,
+    HERIDO,
+    GRAVE,
+    INCONSCIENTE,
+    MUERTO
+
+}

5ce1523f086d17b8f694bec735bdf90729712b1c
Saul <thesimulgamer007@gmail.com>
2025-04-30 14:54:49 +0100

    Update clases.java

--- model/clases.java
+++ model/clases.java
@@ -8,72 +8,67 @@ abstract class perso {
 
     abstract void atacar();
 
+    public perso(String nombre, int nivel, int salud) {
+        this.nombre = nombre;
+        this.nivel = nivel;
+        this.salud = salud;
+    }
 
     public String getNombre() {
         return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
     }
 
     public int getNivel() {
         return nivel;
     }
 
-    public void setNivel(int nivel) {
-        this.nivel = nivel;
-    }
-
     public int getSalud() {
         return salud;
     }
-
-    public void setSalud(int salud) {
-        this.salud = salud;
-    }
-
-
 
     @Override
     public String toString() {
         return "personaje: " + nombre + "nivel: " + nivel + "salud: " + salud;
     }
 
-    //falta clases personalizadas equals() y HashCode() ademas de terminar con las clases de los personajes
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+        result = prime * result + nivel;
+        result = prime * result + salud;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        perso other = (perso) obj;
+        if (nombre == null) {
+            if (other.nombre != null)
+                return false;
+        } else if (!nombre.equals(other.nombre))
+            return false;
+        if (nivel != other.nivel)
+            return false;
+        if (salud != other.salud)
+            return false;
+        return true;
+    }
+
+    
 
 }
 
 public class clases {
 
-    class fisico {
     
-       
-        
-    }
-    class magico {
-    
-       
-        
-    }
-
-    class guerrero extends fisico {
-    
-    
-
-    }
-
-    class mago extends magico {
-
-        
-
-    }
-    
-    class asesino extends fisico {
-
-
-
-    }
 
 
 

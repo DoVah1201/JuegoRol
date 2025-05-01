82dffb83af0082689c8b47709df72e2e43ffcf3f
Marisapr2006 <marisapenarey@gmail.com>
2025-05-02 00:37:06 +0100

    Update juego.java

--- vista/juego.java
+++ vista/juego.java
@@ -7,6 +7,20 @@ public class juego {
 
     public ArrayList<Personaje> personajes = new ArrayList<>();
 
-    
+    public void agregarPersonaje(Personaje p) {
+        personajes.add(p);
+    }
 
+    public void mostrarAcciones() {
+        for (Personaje p : personajes) {
+            System.out.println("" + p);
+            p.atacar();
+
+            if (p instanceof Curable) ((Curable) p).curar();
+            if (p instanceof Volador) ((Volador) p).volar();
+            if (p instanceof Magico) ((Magico) p).lanzarHechizo();
+            if (p instanceof Defendible) ((Defendible) p).defender();
+            if (p instanceof Movilizable) ((Movilizable) p).moverse();
+        }
+    }
 }

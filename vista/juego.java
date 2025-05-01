eaa14c02e74e23795386896a8f0a4c1dab81f05b
Saul <123548756+DoVah1201@users.noreply.github.com>
2025-05-02 00:44:26 +0100

    Update juego.java

--- vista/juego.java
+++ vista/juego.java
@@ -16,11 +16,11 @@ public class juego {
             System.out.println("" + p);
             p.atacar();
 
-            if (p instanceof Curable) ((Curable) p).curar();
-            if (p instanceof Volador) ((Volador) p).volar();
-            if (p instanceof Magico) ((Magico) p).lanzarHechizo();
-            if (p instanceof Defendible) ((Defendible) p).defender();
-            if (p instanceof Movilizable) ((Movilizable) p).moverse();
+            if (p instanceof interfaces.Curar) ((interfaces.Curar) p).Curable();
+            if (p instanceof interfaces.Volar) ((interfaces.Volar) p).VolarAlto();
+            if (p instanceof interfaces.Magico) ((interfaces.Magico) p).LanzarHechizos();
+            if (p instanceof interfaces.Defender) ((interfaces.Defender) p).Defendible();
+            if (p instanceof interfaces.Moverse) ((interfaces.Moverse) p).Movilizable();
         }
     }
 }

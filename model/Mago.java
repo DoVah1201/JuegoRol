b2f9facded9cf03a793436737bd325daed018d64
Marisapr2006 <marisapenarey@gmail.com>
2025-05-02 00:25:51 +0100

    Update Mago.java

--- model/Mago.java
+++ model/Mago.java
@@ -40,7 +40,30 @@ public class Mago extends HabilidadesMagicas implements interfaces.Magico, inter
         return "Mago [mana=" + mana + ", sabiduria=" + sabiduria + "]";
     }
 
-    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + mana;
+        result = prime * result + sabiduria;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!super.equals(obj))
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Mago other = (Mago) obj;
+        if (mana != other.mana)
+            return false;
+        if (sabiduria != other.sabiduria)
+            return false;
+        return true;
+    }
 
     @Override
     public void Curable() {
